d6f766519839674b82d71e52142676e4d8f30f24
Jonathan Souza <jonathanbcsouza@gmail.com>
2024-02-08 16:35:22 +1300

    fix: add background colour for input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -7,12 +7,14 @@ type TInput = {
 };
 
 export const Input = ({ value, type, onChange }: TInput) => {
+  const inputClasses = type === 'todo' ? 'cursor-not-allowed bg-neutral-800' : 'bg-neutral-700';
+
   return (
     <input
       type="text"
       value={value}
       placeholder="add a todo..."
-      className={`text-center p-2 rounded ${type === 'todo' ? 'cursor-not-allowed' : ''}`}
+      className={`text-center p-2 rounded ${inputClasses}`}
       disabled={type === 'todo'}
       autoFocus={type === 'new-todo'}
       onChange={onChange}
